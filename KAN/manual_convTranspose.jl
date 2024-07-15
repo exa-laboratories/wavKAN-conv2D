8b0d91fc4858c7f9ed7e99ac0f9408168d3bcf27
PritRaj1 <pr478@cam.ac.uk>
2024-07-15 19:19:19 +0100

    created CUDA-friendly unfold

--- KAN/manual_convTranspose.jl
+++ KAN/manual_convTranspose.jl
@@ -1,44 +1,19 @@
 module manual_convTranspose
 
+include("../utils.jl")
+
 export KAN_convTranspose2D
 
 using LinearAlgebra
 using Statistics
 using Flux, CUDA, KernelAbstractions, Tullio
+using .UTILS: unfold
 
 function calc_out_dims_transpose(matrix, kernel_size, stride, dilation, padding)
     n, m, n_channels, batch_size = size(matrix)
     h_out = (n - 1) * stride - 2 * padding + dilation * (kernel_size[1] - 1) + 1
     w_out = (m - 1) * stride - 2 * padding + dilation * (kernel_size[2] - 1) + 1
     return h_out, w_out, n_channels, batch_size
-end
-
-function unfold(input, kernel_size; stride=1, padding=0, dilation=1)
-    H, W, C, N = size(input)
-    kh, kw = kernel_size
-    sh, sw = stride isa NTuple ? stride : (stride, stride)
-    ph, pw = padding isa NTuple ? padding : (padding, padding)
-    dh, dw = dilation isa NTuple ? dilation : (dilation, dilation)
-
-    out_h = div(H + 2ph - (dh * (kh - 1) + 1), sh) + 1
-    out_w = div(W + 2pw - (dw * (kw - 1) + 1), sw) + 1
-
-    output = zeros(kh, kw, out_h, out_w, C, N) |> gpu
-
-    padded_input = zeros(H + 2ph, W + 2pw, C, N) |> gpu
-    padded_input[ph + 1:ph + H, pw + 1:pw + W, :, :] = input
-
-    for i in 1:kh
-        for j in 1:kw
-            h_start = (i - 1) * dh + 1
-            w_start = (j - 1) * dw + 1
-            h_indices = h_start:sh:(h_start + sh * (out_h - 1))
-            w_indices = w_start:sw:(w_start + sw * (out_w - 1))
-            output[i, j, :, :, :, :] .= padded_input[h_indices, w_indices, :, :]
-        end
-    end
-
-    return output
 end
 
 function add_padding_transpose(matrix, padding=0)
--- utils.jl
+++ utils.jl
@@ -1,10 +1,11 @@
 module UTILS
 
-export loss_fcn, BIC, UnitGaussianNormaliser, unit_encode, unit_decode, MinMaxNormaliser, minmax_encode, minmax_decode, log_csv, get_grid, node_mul_1D, node_mul_2D
+export loss_fcn, BIC, UnitGaussianNormaliser, unit_encode, unit_decode, MinMaxNormaliser, minmax_encode, minmax_decode, log_csv, get_grid, node_mul_1D, node_mul_2D, unfold
 
 using Statistics
 using Flux
 using CUDA, KernelAbstractions, Tullio
+using NNlib
 
 p = parse(Float32, get(ENV, "p", "2.0"))
 
@@ -82,6 +83,56 @@ function get_grid(x)
     x_reshaped = @tullio y[c, w, h, b] := x[w, h, c, b]
     return vcat(x_reshaped, grid)
     
+end
+
+struct slicer
+    dh
+    dw
+    sh
+    sw
+    out_h
+    out_w
+end
+
+function create_slicer(dh, dw, sh, sw, out_h, out_w)
+    return slicer(dh, dw, sh, sw, out_h, out_w)
+end
+
+function (s::slicer)(input, i, j)
+    h_start = (i - 1) * s.dh + 1
+    w_start = (j - 1) * s.dw + 1
+    h_indices = h_start:s.sh:(h_start + s.sh * (s.out_h - 1))
+    w_indices = w_start:s.sw:(w_start + s.sw * (s.out_w - 1))
+    slice = input[h_indices, w_indices, :, :]
+    return reshape(slice, 1, size(slice)...)
+end
+
+function unfold(input, kernel_size; stride=1, padding=0, dilation=1)
+    H, W, C, N = size(input)
+    kh, kw = kernel_size
+    sh, sw = stride isa NTuple ? stride : (stride, stride)
+    ph, pw = padding isa NTuple ? padding : (padding, padding)
+    dh, dw = dilation isa NTuple ? dilation : (dilation, dilation)
+
+    out_h = div(H + 2ph - (dh * (kh - 1) + 1), sh) + 1
+    out_w = div(W + 2pw - (dw * (kw - 1) + 1), sw) + 1
+
+    slice = create_slicer(dh, dw, sh, sw, out_h, out_w)
+    padded_input = NNlib.pad_circular(input, (ph, ph, pw, pw))
+
+    output = zeros(1, kw, out_h, out_w, C, N) |> gpu
+
+    for i in 1:kh
+        inner_output = zeros(1, out_h, out_w, C, N) |> gpu
+        for j in 1:kw
+            inner_output = cat(inner_output, slice(padded_input, i, j), dims=1)
+        end
+        inner_output = inner_output[2:end, :, :, :, :]
+        inner_output = reshape(inner_output, 1, size(inner_output)...)
+        output = cat(output, inner_output, dims=1)
+    end
+
+    return output[2:end, :, :, :, :, :]
 end
 
 # This is a node of the KAN. It sums wavelets in a manner presented by the wavKAN paper.
