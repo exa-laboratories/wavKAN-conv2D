3f88eda8497bc8cbdd368abd84e8df2780d65d3c
PritRaj1 <pr478@cam.ac.uk>
2024-07-19 03:49:53 +0000

    fixed hps

--- hp_parsing.jl
+++ hp_parsing.jl
@@ -5,7 +5,7 @@ export set_hyperparams
 using ConfParser
 
 function set_CNN_params()
-    conf = ConfParse("MLP_CNN/CNN_config.ini")
+    conf = ConfParse("src/models/MLP_CNN/CNN_config.ini")
     parse_conf!(conf)
 
     ENV["p"] = retrieve(conf, "Loss", "p")
@@ -23,7 +23,7 @@ function set_CNN_params()
 end
 
 function set_FNO_params()
-    conf = ConfParse("MLP_FNO/FNO_config.ini")
+    conf = ConfParse("src/models/MLP_FNO/FNO_config.ini")
     parse_conf!(conf)
 
     ENV["p"] = retrieve(conf, "Loss", "p")
@@ -44,7 +44,7 @@ function set_FNO_params()
 end
 
 function set_KAN_CNN_params()
-    conf = ConfParse("wavKAN_CNN/KAN_CNN_config.ini")
+    conf = ConfParse("src/models/wavKAN_CNN/KAN_CNN_config.ini")
     parse_conf!(conf)
 
     ENV["p"] = retrieve(conf, "Loss", "p")
