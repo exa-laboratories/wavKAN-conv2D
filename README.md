34ae2c275e812ab7b13f48511f3a7ad5256450ed
PritRaj1 <pr478@cam.ac.uk>
2024-07-19 03:26:45 +0000

    optimised, didn't work well

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # wavKAN-conv2D
 A Julia implementation of Wavelet Kolmogorov-Arnold Networks (wavKAN) for Convolutional Neural Networks.  Mutli-layer Perceptron (MLP) and wavKAN implementations of the Convolutional Neural Network (CNN) and Fourier Neural Operator (FNO) are applied to learn the solution operator of the 2D Darcy Flow Problem.
 
-The MLP models were developed in a [previous side project](https://github.com/PritRaj1/Neural-Operator-Learning). The commit history attributed to their development can be found there. The Dense KAN layer was also developed in a [previous side project](https://github.com/PritRaj1/Julia-Wav-KAN).
+The MLP models were developed in a [previous side project](https://github.com/PritRaj1/Neural-Operator-Learning). The commit history attributed to their development can be found there. The Dense KAN layer was also developed in a [previous project](https://github.com/PritRaj1/Julia-Wav-KAN).
 
 ## Predictions
 <p align="center">
--- predict_flow.jl
+++ predict_flow.jl
@@ -1,6 +1,7 @@
 include("src/pipeline/data_processing/data_loader.jl")
 include("src/models/MLP_CNN/CNN.jl")
 include("src/models/MLP_FNO/FNO.jl")
+include("src/models/wavKAN_CNN/KAN_CNN.jl")
 
 using Plots; pythonplot()
 using Flux
@@ -10,12 +11,12 @@ using .loaders: get_darcy_loader
 
 train_loader, test_loader = get_darcy_loader(1)
 
-MODEL_NAME = "CNN"
+MODEL_NAME = "MLP CNN"
 
 model_file = Dict(
-    "CNN" => "src/models/MLP_CNN/logs/trained_models/model_1.bson",
-    "FNO" => "src/models/MLP_FNO/logs/trained_models/model_1.bson",
-    "KAN_CNN" => "src/models/wavKAN_CNN/logs/trained_models/model_1.bson"
+    "MLP CNN" => "src/models/MLP_CNN/logs/trained_models/model_1.bson",
+    "MLP FNO" => "src/models/MLP_FNO/logs/trained_models/model_1.bson",
+    "KAN CNN" => "src/models/wavKAN_CNN/logs/trained_models/model_1.bson"
 )[MODEL_NAME]
 
 # Load the model
@@ -24,14 +25,27 @@ model_file = Dict(
 # Move the model to the GPU
 model = gpu(model)
 
-# Plot the prediction
 X, Y = [x for x in range(0, stop=1, length=32)], [y for y in range(0, stop=1, length=32)]
 
+# Plot the prediction
 anim = @animate for (a, u) in test_loader
     u_pred = model(a) |> cpu
     u_pred = u_pred[:, :, 1, 1]
-    contourf(X, Y, u_pred, title="$MODEL_NAME Prediction", cbar=false, color=:viridis)
+    contourf(X, Y, u_pred, title="$MODEL_NAME Prediction", cbar=false, color=:viridis, aspect_ratio=:equal)
 end
 
 # Save the animation to file
 gif(anim, "figures/$MODEL_NAME" * "_prediction.gif", fps=5)
+
+# Plot the error field
+anim = @animate for (a, u) in test_loader
+    u_pred = model(a) |> cpu
+    u_pred = u_pred[:, :, 1, 1]
+    u = u |> cpu
+    u = u[:, :, 1, 1]
+    contourf(X, Y, u_pred .- u, title="$MODEL_NAME Error Field", cbar=false, color=:viridis, aspect_ratio=:equal)
+end
+
+# Save the animation to file
+gif(anim, "figures/$MODEL_NAME" * "_error.gif", fps=5)
+
